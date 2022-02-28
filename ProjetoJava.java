07f9bb256071a5082f7841865d0e5f59335a9ab9
JacielleMarques <jacymarques71@gmail.com>
2022-02-28 18:45:32 -0300

    projeto

---
+++ ProjetoJava.java
@@ -0,0 +1,9 @@
+public class ProjetoJava {
+
+	public static void main(String[] args){
+		System.out.println("Laboratório de Programação 2 – LP2"); // Imprimindo na tela o que digitou//
+		System.out.println("2 ano de informática");
+		System.out.println("Seja bem vindo!");
+		System.out.println("Vamos aprender a matéria mais chata que e Java.");
+	}
+}
